726d5a52050f2a7883d9535fa6532f7eab4e6588
Denis Stalz-John <denis.stalz-john@codecentric.de>
2020-05-18 19:32:44 +0200

    small changes

--- ccmlutils/procedures/defaulttrain.py
+++ ccmlutils/procedures/defaulttrain.py
@@ -1,11 +1,21 @@
-from typing import Optional
+from typing import Optional, Callable
 
 from keras import Model
+from keras.losses import Loss
+from keras.optimizers import Optimizer
 
 
-def fit_generator(model: Model, train_set, validation_set, epochs: int, callbacks=None,
+def fit_generator(model: Callable[[], Model], train_set, validation_set, epochs: int,
+                  optimizer: Optimizer,
+                  loss: Loss,
+                  callbacks=None,
                   steps_per_epoch: Optional[int] = None,
-                  validation_steps: Optional[int] = None):
+                  validation_steps: Optional[int] = None,
+                  ):
+
+    model = model()
+    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
     history = model.fit_generator(train_set,
                                   epochs=epochs,
                                   validation_data=validation_set,
@@ -13,4 +23,4 @@ def fit_generator(model: Model, train_set, validation_set, epochs: int, callback
                                   validation_steps=validation_steps,
                                   steps_per_epoch=steps_per_epoch)
 
-    return dict(model=model, history=history)
+    return dict(history=history)
