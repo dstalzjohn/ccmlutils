45895e73c2b7f86257e34a3faf349e844a115f7f
Denis Stalz-John <denis.stalz-john@codecentric.de>
2020-05-25 10:17:21 +0200

    quick fix for keras issue

--- ccmlutils/procedures/defaulttrain.py
+++ ccmlutils/procedures/defaulttrain.py
@@ -3,6 +3,8 @@ from typing import Optional, Callable
 from tensorflow.keras.losses import Loss
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Optimizer
+from tensorflow.keras.callbacks import Callback
+from tensorflow.keras.utils import Sequence
 
 
 def fit_generator(model: Callable[[], Model], train_set, validation_set, epochs: int,
@@ -15,7 +17,13 @@ def fit_generator(model: Callable[[], Model], train_set, validation_set, epochs:
 
     model = model()
     model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
-
+    # This callback is only temporary till issue in tf.keras is fixed where
+    # Sequence.on_epoch_end() is not called correctly
+    cb = DataSetOnEndCallback(train_set)
+    if callbacks is None:
+        callbacks = [cb]
+    else:
+        callbacks.append(cb)
     history = model.fit_generator(train_set,
                                   epochs=epochs,
                                   validation_data=validation_set,
@@ -24,3 +32,13 @@ def fit_generator(model: Callable[[], Model], train_set, validation_set, epochs:
                                   steps_per_epoch=steps_per_epoch)
 
     return dict(history=history.history)
+
+
+class DataSetOnEndCallback(Callback):
+
+    def __init__(self, dataset: Sequence):
+        super().__init__()
+        self.dataset = dataset
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.dataset.on_epoch_end()
