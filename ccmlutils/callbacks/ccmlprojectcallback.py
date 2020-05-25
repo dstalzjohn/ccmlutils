3f0da4d9ade24ea7cd2400d4d292d4cf40b2eaf7
Denis Stalz-John <denis.stalz-john@codecentric.de>
2020-05-25 09:22:58 +0200

    added exp prefix

--- ccmlutils/callbacks/ccmlprojectcallback.py
+++ ccmlutils/callbacks/ccmlprojectcallback.py
@@ -8,9 +8,9 @@ from ccmlutils.callbacks.gitversioncallback import produce_git_version_yaml
 from ccmlutils.config import envconfig as envc
 
 
-def produce_project_info(filepath: str):
+def produce_project_info(filepath: str, exp_prefix: str or None):
     out_dict = {envc.RUN_ID_KEY: envc.get_run_id(), envc.SHORT_ID_KEY: envc.get_short_id(),
-                envc.EXP_NAME_KEY: envc.get_exp_name()}
+                envc.EXP_NAME_KEY: envc.get_exp_name(), envc.EXP_PREFIX_KEY: exp_prefix}
     with open(filepath, 'w') as outfile:
         yaml.dump(out_dict, outfile, default_flow_style=False)
 
@@ -21,7 +21,8 @@ class CCMLProjectCallback(Callback):
                  git_version_filename: str = "git_versions.yml",
                  project_info_filename: str = "project_info.yml",
                  enable_train_logging: bool = True,
-                 train_log_file: str = "train_logs.csv"):
+                 train_log_file: str = "train_logs.csv",
+                 exp_prefix: str = None):
         super().__init__()
         self.git_dirs: List[str] = [git_dirs] if type(git_dirs) is str else git_dirs
         self.git_modules: List[str] = [git_modules] if type(git_modules) is str else git_modules
@@ -29,6 +30,7 @@ class CCMLProjectCallback(Callback):
         self.git_version_filename: str = git_version_filename
         self.project_info_filename: str = project_info_filename
         self.log_callback: Optional[Callback] = None
+        self.exp_prefix: str = exp_prefix
         if enable_train_logging:
             self.log_callback = CSVLogger(filename=join(log_path, train_log_file))
 
@@ -39,7 +41,7 @@ class CCMLProjectCallback(Callback):
 
     def on_train_begin(self, logs=None):
         produce_git_version_yaml(join(self.log_path, self.git_version_filename), self.git_dirs, self.git_modules)
-        produce_project_info(join(self.log_path, self.project_info_filename))
+        produce_project_info(join(self.log_path, self.project_info_filename), exp_prefix=self.exp_prefix)
         if self.log_callback is not None:
             self.log_callback.on_train_begin(logs=logs)
 
